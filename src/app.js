174538ba3641f2e60ac81189e3aab72900ebd902
Meet Patel <meetaiims@yahoo.com>
2020-11-08 01:25:30 -0500

    After modularizing error handler middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const errorhandler = require('./error-handler');
 
 const app = express();
 
@@ -19,16 +20,6 @@ app.get('/', (req, res) => {
     res.send('Hello, boilerplate!');
 });
 
-app.use(errorHandler = (error, req, res, next) => {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error'}};
-    } else {
-        console.error(error);
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response);
-
-})
+app.use(errorhandler)
 
 module.exports = app;
