631f63e2cba634a6255186e2f081efaea3276fe3
Meet Patel <meetaiims@yahoo.com>
2020-11-08 01:10:35 -0500

    After adding authroization middleware

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_TOKEN: process.env.API_TOKEN || 72c42304-1f0e-11eb-adc1-0242ac120002
+    API_TOKEN: process.env.API_TOKEN || '72c42304-1f0e-11eb-adc1-0242ac120002'
 };
