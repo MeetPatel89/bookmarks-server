211acde3ece67244dc8da16c59d6cdab8e904e48
Meet Patel <meetaiims@yahoo.com>
2020-11-08 00:37:19 -0500

    Added and layered logger

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 72c42304-1f0e-11eb-adc1-0242ac120002
-}
+};
