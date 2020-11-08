e946c76903db8bedc7c77c492374e447e6c3e7f3
Meet Patel <meetaiims@yahoo.com>
2020-11-08 01:30:16 -0500

    Setting root GET req and testing server

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const errorhandler = require('./error-handler');
+const validateBearerToken = require('./validate-api-token');
 
 const app = express();
 
@@ -12,14 +13,17 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
+// Middleware functions
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
+app.use(errorhandler)
 
 app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!');
+    res.send('Hello, bookmarker!');
 });
 
-app.use(errorhandler)
+
 
 module.exports = app;
