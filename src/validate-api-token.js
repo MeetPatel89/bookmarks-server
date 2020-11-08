35ba6401174d49eb3e8fe8f1294cc97ca41d0a01
Meet Patel <meetaiims@yahoo.com>
2020-11-08 01:36:30 -0500

    Iterative debugging

--- src/validate-api-token.js
+++ src/validate-api-token.js
@@ -4,7 +4,7 @@ const logger = require('./logger');
 const validateBearerToken = (req, res, next) => {
     const authToken = req.get('Authorization');
     
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
         logger.error(`Unauthorized request to path: ${req.path}`)
         return res.status(401).json({ error: 'Unauthorized request' });
     };
@@ -13,3 +13,5 @@ const validateBearerToken = (req, res, next) => {
     next();
 
 }
+
+module.exports = validateBearerToken;
